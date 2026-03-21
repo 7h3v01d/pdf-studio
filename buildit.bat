01dd6d87eb03abdd417e039aa9869dd4f1064084
Leon Priest <z78c4t4xk7@privaterelay.appleid.com>
2026-03-21 20:16:12 +1000

    Add files via upload

--- buildit.bat
+++ buildit.bat
@@ -1,3 +1,3 @@
 cd src
-pyinstaller --onefile --windowed --icon=icon.ico --name="PDF Reader Pro" --add-data "icon.ico;." pdf_reader.py
+pyinstaller --onefile --windowed --icon=icon.ico --name="PDF Reader Pro" --add-data "icon.ico;." --exclude-module tensorflow --exclude-module torch --exclude-module matplotlib --exclude-module scipy --exclude-module pandas --exclude-module numpy --exclude-module IPython --exclude-module pygame --exclude-module PIL pdf_reader.py
 pause
